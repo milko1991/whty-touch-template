d3dba96a38c5900c09099c63adacd23e62dc86d4
yigeng <yigengwork@gmail.com>
2018-12-26 09:29:32 +0800

    feat 主题配置文件修改

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     useEslint: true,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
-    showEslintErrorsInOverlay: false,
+    showEslintErrorsInOverlay: true,
 
     /**
      * Source Maps
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
--- src/utils/apiRequestHandler.js
+++ src/utils/apiRequestHandler.js
@@ -34,70 +34,8 @@ AxiosInstance.interceptors.response.use(
     return response;
   },
   err => {
-    console.log(err);
     return Promise.reject(err);
   }
 );
 
-/**
- *
- * @param {*Object} dataConfig
- * {
- *   url: String 请求路径
- *   method: String 请求类型
- *   payload: Object 请求参数
- * }
- * @return {*Promise} 返回Promise对象、可在then中处理结果，在catch中捕捉错误
- */
-const apiRequestHandler = function(dataConfig) {
-  return new Promise((resolve, reject) => {
-    /**
-     * 默认axios配置
-     * 1.根路径
-     * 2.超时时间
-     * 3.请求路径（这里的url等于 根路径baseURL加上传入的路径）
-     */
-    let DEFAULT_AXIOS_CONFIG = {
-      url: dataConfig.url,
-      method: dataConfig.method,
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    };
-    /**
-     *
-     * 1.t 请求时间戳、防止浏览器缓存，保证获取到最新数据
-     */
-    let DEFAULT_PARAMS = {
-      t: +new Date()
-    };
-
-    /**
-     * 区别get和post请求 做不同配置
-     * 1.get  携带参数写法为：params:{}
-     * 2.post 携带参数写法为：data：{}
-     */
-    if (dataConfig.method.toLowerCase() === 'get' || dataConfig.method.toLowerCase() === 'delete') {
-      let params = Object.assign({}, DEFAULT_PARAMS, dataConfig.payload);
-      DEFAULT_AXIOS_CONFIG = Object.assign({}, DEFAULT_AXIOS_CONFIG, {
-        params
-      });
-    } else {
-      let data = Object.assign({}, DEFAULT_PARAMS, dataConfig.payload);
-      DEFAULT_AXIOS_CONFIG = Object.assign({}, DEFAULT_AXIOS_CONFIG, {
-        data
-      });
-    }
-
-    AxiosInstance(DEFAULT_AXIOS_CONFIG)
-      .then(res => {
-        resolve(res.data);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-};
-
-export default apiRequestHandler;
+export default AxiosInstance;
